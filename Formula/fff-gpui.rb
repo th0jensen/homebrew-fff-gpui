482406d2a3431114382625a4411832ab3f783f53
Thomas Jensen <thomas.jensen_@outlook.com>
2026-05-04 01:19:18 +0200

    chore: update to v0.1.2

--- Formula/fff-gpui.rb
+++ Formula/fff-gpui.rb
@@ -1,10 +1,10 @@
 class FffGpui < Formula
   desc "System-wide file finder for macOS built on GPUI, with Zed integration"
   homepage "https://github.com/th0jensen/fff-gpui"
-  url "https://github.com/th0jensen/fff-gpui/releases/download/v0.1.1/fff-gpui"
-  sha256 "051671d7bb01ff7f6475e3bbadca9c3efc11cf77d0382d7765e4cef03e024020"
+  url "https://github.com/th0jensen/fff-gpui/releases/download/v0.1.2/fff-gpui"
+  sha256 "d4952e56cefb4e0b9760cb3e89be12d65f8db78f574a487b25ec6024752524b6"
   license "MIT"
-  version "0.1.1"
+  version "0.1.2"
 
   depends_on :macos
 
